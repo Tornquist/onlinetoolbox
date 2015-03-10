3583bd26472d15e93f9ed57b90c12076cf7b324f
Nathan Tornquist <Tornquist@live.com>
2015-03-09 22:52:40 -0400

    Added devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,8 @@ Rails.application.routes.draw do
     get :sections, on: :member
   end
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  #devise_for :users
   get 'users/favorite_instruments/', to: 'favorite_instruments#user', as: 'user_favorite_instruments'
   post 'users/favorite_instruments/', to: 'favorite_instruments#create_multiple', as: 'create_multiple_user_favorite_instruments'
 
