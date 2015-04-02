23761d06882ec22d21cdc50a6abaad3142b08d35
Nathan Tornquist <Tornquist@live.com>
2015-04-02 19:48:07 -0400

    Added favorites to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_favorites
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_breadcrumb "Home", :root_path
+
+  def set_favorites
+    @favorites = Favorite.where(:user_id => current_user.id, :active => true).reverse
+  end
 end
