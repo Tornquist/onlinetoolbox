2da23819e5390776b73f1efce834cd3913f0035d
Nathan Tornquist <Tornquist@live.com>
2015-05-27 15:06:06 -0500

    Basic award index layout made

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,8 +1,13 @@
 class AwardsController < ApplicationController
+  add_breadcrumb "Awards", :awards_path
+
+  def index
+
+  end
+
   def student_view
     @student = Student.find(params[:id])
-    add_breadcrumb @student.full_name, :edit_student_path
-    add_breadcrumb "Awards", :awards_student_path
+    add_breadcrumb @student.full_name, :awards_student_path
     @student_ribbons = StudentRibbon.where(student: @student)
     @office_id = StudentOffice.where(student: @student).first.try(:office).try(:id)
   end
