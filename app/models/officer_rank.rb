4c68b433dcd413ef5757a2b91b2e427ae25b7079
Nathan Tornquist <Tornquist@live.com>
2015-05-27 15:25:28 -0500

    Added adjusted inventory and am ready for search page with awards

--- app/models/officer_rank.rb
+++ app/models/officer_rank.rb
@@ -1,3 +1,16 @@
 class OfficerRank < ActiveRecord::Base
   has_many :student_officer_ranks
+
+  def adjusted_inventory
+    adjust = 0
+    student_officer_ranks.each do |student|
+      if (student.date_issued != nil)
+        adjust -= 1;
+      end
+      if (student.date_returned != nil)
+        adjust += 1;
+      end
+    end
+    inventory + adjust
+  end
 end
