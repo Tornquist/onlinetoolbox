f9e8f7fcf68007704f79bdb9ef47f3f9c89d0154
Nathan Tornquist <Tornquist@live.com>
2015-05-27 14:33:02 -0500

    Added confirmation dialog

--- app/assets/javascripts/awards.js
+++ app/assets/javascripts/awards.js
@@ -28,26 +28,30 @@ awards_ready = function() {
 
   if ($('.student_rank').length) {
     $('.student_rank').click( function() {
+
       type = "";
       if ($(this).hasClass("approve")) { type = "approve"; }
       else if ($(this).hasClass("issue")) { type = "issue"; }
       else if ($(this).hasClass("return")) { type = "return"; }
 
-      student_id = $('#student_id')[0].value;
-      officer_rank_id = $(this).parent().siblings('#officer_rank_id').val();
+      if (confirm("Are you sure you wish to "+type+" this rank? This action is irreversible")) {
+        student_id = $('#student_id')[0].value;
+        officer_rank_id = $(this).parent().siblings('#officer_rank_id').val();
 
-      $.ajax({
-        type: 'POST',
-        url: '/student_officer_ranks/create_update',
-        dataType: 'json',
-        data: { student_officer_rank: {officer_rank_id: officer_rank_id, student_id: student_id, type: type } }
-      });
+        $.ajax({
+          type: 'POST',
+          url: '/student_officer_ranks/create_update',
+          dataType: 'json',
+          data: { student_officer_rank: {officer_rank_id: officer_rank_id, student_id: student_id, type: type } }
+        });
 
-      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      d = new Date();
-      pretty_date = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        d = new Date();
+        pretty_date = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
 
-      $(this).replaceWith(pretty_date);
+        $(this).replaceWith(pretty_date);
+      }
+      else { }
     });
   }
 
