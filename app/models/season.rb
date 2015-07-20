b0b4c2f39751b87c0fcb4c5e244e4927ed6ceb37
Nathan Tornquist <Tornquist@live.com>
2015-07-19 19:38:08 -0500

    Students is now active record association

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,14 +1,11 @@
 class Season < ActiveRecord::Base
   has_many :games
   has_many :sections
+  has_many :students, through: :sections
   accepts_nested_attributes_for :games, :allow_destroy => true
   accepts_nested_attributes_for :sections, :allow_destroy => true
 
   def members
     sections.map{|s| s.section_members.reject{ |i| i.student.archive}.size }.reduce(:+)
-  end
-
-  def students
-    sections.map(&:students).flatten(1).sort { |a, b| a.full_name_reversed.downcase <=> b.full_name_reversed.downcase }
   end
 end
